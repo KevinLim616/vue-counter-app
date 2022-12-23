424881a190f16f4240c13eb20ec50744c7d9f492
KevinLim616 <kevinlim198@gmail.com>
2022-12-23 12:39:16 +0800

    finished todo app

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,158 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+
+const showModal = ref(false);
+
+const triggerModal = () => {
+  showModal.value = !showModal.value;
+};
+
+const newNote = ref("");
+const notes = ref([]);
+
+const getRandomColor = () => {
+  let color;
+  color = "hsl(" + Math.random() * 360 + ",100%,75%)";
+  return color;
+};
+
+const addNote = () => {
+  if (newNote.value.length > 9) {
+    notes.value.push({
+      id: Math.floor(Math.random * 1000000),
+      text: newNote.value,
+      date: new Date(),
+      backgroundColor: getRandomColor(),
+    });
+    showModal.value = false;
+    newNote = "";
+  } else {
+    alert("note needs to be at least 10 characters.");
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="overlay" v-if="showModal">
+      <div class="modal">
+        <textarea
+          v-model.trim="newNote"
+          name="note"
+          id="note"
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button @click="addNote()">Add Note</button>
+        <button class="close" @click="triggerModal()">Close</button>
+      </div>
+    </div>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button class="header-button" @click="triggerModal()">+</button>
+      </header>
+      <div class="card-container">
+        <div
+          class="card"
+          v-for="note in notes"
+          :key="note.id"
+          :style="{ backgroundColor: note.backgroundColor }"
+        >
+          <p class="main-text">
+            {{ note.text }}
+          </p>
+          <p class="date">{{ note.date.toLocaleDateString("en-UK") }}</p>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+}
 header {
-  line-height: 1.5;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
+.header-button {
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: rgb(21, 20, 20);
+  border-radius: 100%;
+  color: white;
+  font-size: 20px;
+}
+.card {
+  width: 225px;
+  height: 225px;
+  background-color: rgb(200, 37, 182, 44);
+  padding: 10px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+.date {
+  font-size: 12.5px;
+  font-weight: bold;
+}
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.modal {
+  width: 750px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.modal button {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: blueviolet;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin-top: 15px;
+}
+.modal .close {
+  background-color: rgb(193, 15, 15);
+  margin-top: 7px;
 }
 </style>
